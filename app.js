ac844c0214d61c50b5dfab038e36329274fe2aae
ZhaoHouLin <rodes5292@gmail.com>
2019-10-04 20:49:33 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -16,38 +16,25 @@ const User = db.User
 app.engine("handlebars", exphbs({ defaultLayout: 'main' }))
 app.set("view engine", "handlebars")
 
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
-// 首頁
-app.get('/', (req, res) => {
-  res.send('hello world')
+app.use(session({
+  secret: 'your secret key',
+  resave: 'false',
+  saveUninitialized: 'false',
+}))
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize())
+app.use(passport.session())
+require('./config/passport')(passport)
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  next()
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
+app.use('/users', require('./routes/user'))
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
