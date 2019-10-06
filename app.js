0a31542819905742e2fb195d0b39c0c869635533
ZhaoHouLin <rodes5292@gmail.com>
2019-10-06 10:10:59 +0800

    Add connect-flash for UI messages

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express()
 const port = 3000
 const session = require('express-session')
 const passport = require('passport')
+const flash = require('connect-flash')             // 載入 connect-flash   
+
 
 if (process.env.NODE_ENV !== 'production') {      // 如果不是 production 模式
   require('dotenv').config()                      // 使用 dotenv 讀取 .env 檔案
@@ -37,10 +39,25 @@ app.use((req, res, next) => {
   next()
 })
 
+
+app.use(flash())
+// 建立 local variables
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.isAuthenticated = req.isAuthenticated()    // 辨識使用者是否已經登入的變數，讓 view 可以使用
+
+  // 新增兩個 flash message 變數 
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
+
 app.use('/', require('./routes/home'))
 app.use('/users', require('./routes/user'))
 app.use('/todos', require('./routes/todo'))
 app.use('/auth', require('./routes/auths'))
+
+
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}`)
